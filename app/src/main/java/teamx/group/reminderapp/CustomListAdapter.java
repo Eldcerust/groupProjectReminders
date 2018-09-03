211573718fabecc84dbc344108a5fb7ddde3dfbe
Alvin Thio <alvinthiozhang@gmail.com>
2018-09-03 23:16:58 +0800

    Done some custom list parts

--- app/src/main/java/teamx/group/reminderapp/CustomListAdapter.java
+++ app/src/main/java/teamx/group/reminderapp/CustomListAdapter.java
@@ -1,10 +1,15 @@
 package teamx.group.reminderapp;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.widget.
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,7 +20,7 @@ public class CustomListAdapter extends BaseAdapter{
 
     public CustomListAdapter(Context context_required,ArrayList<RemindersModel> reminder_listed){
         this.reminder_lists=reminder_listed;
-        inflated_layout=LayoutInflater.from(context_required.getApplicationContext());
+        this.inflated_layout=LayoutInflater.from(context_required.getApplicationContext());
     }
 
     @Override
@@ -35,9 +40,16 @@ public class CustomListAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        ViewHolder view_lists;
+        if(convertView==null){
+            convertView=this.inflated_layout.inflate(R.layout.list_row_layout,null);
+            view_lists=new ViewHolder();
+        }
     }
 
-    @Override
-    pulbi
+    static class ViewHolder{
+        TextView name_view;
+        TextView time_view;
+        TextView date_view;
+    }
 }
