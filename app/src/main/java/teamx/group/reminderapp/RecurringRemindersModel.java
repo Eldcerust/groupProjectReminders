89822bc46cc6a0efa2db5cbb8433d39104c625b6
eldcerust <alvinthiozhang@gmail.com>
2018-09-25 17:12:56 +0800

    More updates

--- app/src/main/java/teamx/group/reminderapp/RecurringRemindersModel.java
+++ app/src/main/java/teamx/group/reminderapp/RecurringRemindersModel.java
@@ -3,6 +3,7 @@ package teamx.group.reminderapp;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.UUID;
 
 public class RecurringRemindersModel extends RemindersModel{
     private int number_of_repetition;
@@ -29,6 +30,56 @@ public class RecurringRemindersModel extends RemindersModel{
         this.number_of_repetition=0;
         this.days_of_repetition=0;
         this.micro_reminders=false;
+    }
+
+    @Override
+    public void set_list(ArrayList<CheckBoxListSingle> list_set) {
+        super.set_list(list_set);
+    }
+
+    @Override
+    public void set_reminder_date_time(Calendar date) {
+        super.set_reminder_date_time(date);
+    }
+
+    @Override
+    public void set_reminder_voice_profile(VoiceProfileModel voice_profile) {
+        super.set_reminder_voice_profile(voice_profile);
+    }
+
+    @Override
+    public void set_reminder_UUID(UUID uuid) {
+        super.set_reminder_UUID(uuid);
+    }
+
+    @Override
+    public String get_reminder_name() {
+        return super.get_reminder_name();
+    }
+
+    @Override
+    public Calendar get_reminder_date_time() {
+        return super.get_reminder_date_time();
+    }
+
+    @Override
+    public UUID get_reminder_UUID() {
+        return super.get_reminder_UUID();
+    }
+
+    @Override
+    public VoiceProfileModel get_reminder_voice_profile() {
+        return super.get_reminder_voice_profile();
+    }
+
+    @Override
+    public ArrayList<CheckBoxListSingle> get_checkbox_list() {
+        return super.get_checkbox_list();
+    }
+
+    @Override
+    public void set_reminder_name(String string) {
+        super.set_reminder_name(string);
     }
 
     public int get_days_of_repetition(){return this.days_of_repetition;}
